44ccc39467169e22daa56007ce139bfe25b57ad7
VZbitnieva <v.zbitnieva@gmail.com>
2023-07-06 15:11:23 +0300

    some more fixes

--- RUBY/Part_I/lesson1/2_hello.rb
+++ RUBY/Part_I/lesson1/2_hello.rb
@@ -7,7 +7,13 @@ require 'bundler/setup'
 Bundler.require(:default)
 
 I18n.load_path << Dir["#{File.expand_path('../../config/locales')}/*.yml"]
-I18n.default_locale = PARSED_LANGUAGE
+I18n.default_locale = :en
+
+language = PARSED_LANGUAGE || I18n.default_locale
+
+I18n.locale = language
+
+I18n.fallbacks = [I18n.default_locale]
 
 print "#{I18n.t('enter.name')}: "
 my_name = gets.chomp
