69728439d5d95094368717b94bc6c06dac8ab75d
VZbitnieva <v.zbitnieva@gmail.com>
2023-07-10 11:17:39 +0300

    User class has been expanded

---
+++ RUBY/Part_I/lesson2/3_user.rb
@@ -0,0 +1,2 @@
+require_relative '../../lib/i18n_setup'
+require_relative "../../lib/classes/user"
--- RUBY/lib/classes/user.rb
+++ RUBY/lib/classes/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User
-  attr_accessor :name
+  attr_accessor :name, :full_name, :professon
 
   def initialize(name)
     @name = name
