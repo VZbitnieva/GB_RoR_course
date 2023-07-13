7a94e800174e9c11dc3737dc2c514240f604710d
VZbitnieva <v.zbitnieva@gmail.com>
2023-07-13 22:19:54 +0300

    100.00% documented
    100

--- RUBY/Part_I/lesson2/4_user_file.rb
+++ RUBY/Part_I/lesson2/4_user_file.rb
@@ -9,7 +9,7 @@ user.full_name = gets.chomp
 print "#{I18n.t('enter.profession')}: "
 user.profession = gets.chomp
 
-File.open('../../doc/user.txt', 'w') do |file|
+File.open('../../tmp/user.txt', 'w') do |file|
   file.puts " #{I18n.t('phrases.user')}:
             #{I18n.t('phrases.full_name')}: #{user.full_name}
             #{I18n.t('phrases.profession')}: #{user.profession}"
--- RUBY/Part_I/lesson3/2_user_rdoc.rb
+++ RUBY/Part_I/lesson3/2_user_rdoc.rb
@@ -1,15 +1,35 @@
 # frozen_string_literal: true
 
-# = User Documentation
-
-#    == Description
-#    This program demonstrates the use of the User class. It allows you to set and retrieve user information such as name, full name, and profession.
+##
+# The User class represents the user of the system.
 class User
-  attr_accessor :full_name, :profession
+  # Returns the full name of the user.
+  # Public: Set the full name of the user or request information output
+  #
+  # full_name - A String representing the full name of the user.
+  #
+  # = Examples
+  #
+  #   user.full_name = 'Shevchenko Taras Grygorovych'
+  #   # => "Shevchenko Taras Grygorovych"
+  #   user.full_name # "Shevchenko Taras Grygorovych"
+
+  attr_accessor :full_name
+  # Returns the profession of the user.
+  # Public: Set the profession of the user or request information output
+  #
+  # profession - A String representing the profession of the user.
+  #
+  # = Examples
+  #
+  #   user.profession = 'poet'
+  #   # => "poet"
+  #   user.profession # "poet"
+  attr_accessor :profession
 
   # Public: Initialize a new User object.
   #
-  # Examples
+  # = Examples
   #
   #   user = User.new
   #   # => #<User:0x00007fb30513b640 @full_name="", @profession="">
@@ -23,7 +43,7 @@ end
 
 # Public: Create a new User object.
 #
-# Examples
+# = Examples
 #
 #   user = User.new
 #   # => #<User:0x00007fb30513b640 @full_name="", @profession="">
@@ -33,7 +53,7 @@ user = User.new
 #
 # full_name - A String representing the full name of the user.
 #
-# Examples
+# = Examples
 #
 #   user.full_name = 'Shevchenko Taras Grygorovych'
 #   # => "Shevchenko Taras Grygorovych"
@@ -55,7 +75,7 @@ user.profession = 'poet'
 
 # Public: Print the user information.
 #
-# Examples
+# = Examples
 #
 #   puts "User:
 #         Full name: #{user.full_name}
