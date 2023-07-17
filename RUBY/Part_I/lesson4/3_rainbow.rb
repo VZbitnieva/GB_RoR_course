8b4c86252c97d0c71fb4483b1499e32e27f4a3c1
VZbitnieva <v.zbitnieva@gmail.com>
2023-07-17 12:16:36 +0300

    translate the colors of the rainbow

---
+++ RUBY/Part_I/lesson4/3_rainbow.rb
@@ -0,0 +1,5 @@
+require_relative '../../lib/constants'
+
+colors = RAINBOW_EN.zip(RAINBOW).to_h
+
+puts colors.inspect
--- RUBY/lib/constants.rb
+++ RUBY/lib/constants.rb
@@ -3,3 +3,5 @@
 require_relative './i18n_setup'
 
 DAYS_OF_THE_WEEK = I18n.t('date.day_names')
+RAINBOW = I18n.t('rainbow')
+RAINBOW_EN = I18n.t('rainbow', locale: :en)
