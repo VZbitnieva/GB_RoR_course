42df24af75b90bb1b08a96c29d1a101334fff9b7
VZbitnieva <v.zbitnieva@gmail.com>
2023-07-19 12:10:57 +0300

    display the days of the week

--- RUBY/lib/constants.rb
+++ RUBY/lib/constants.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 require_relative './i18n_setup'
-
+SUNDAY = I18n.t('date.day_names')[0]
+MONDAY = I18n.t('date.day_names')[1]
+TUESDAY = I18n.t('date.day_names')[2]
+WEDNESDAY = I18n.t('date.day_names')[3]
+THURSDAY = I18n.t('date.day_names')[4]
+FRIDAY = I18n.t('date.day_names')[5]
+SATURDAY = I18n.t('date.day_names')[6]
+WEEK = [SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY]
 DAYS_OF_THE_WEEK = I18n.t('date.day_names')
 RAINBOW = I18n.t('rainbow')
 RAINBOW_EN = I18n.t('rainbow', locale: :en)
