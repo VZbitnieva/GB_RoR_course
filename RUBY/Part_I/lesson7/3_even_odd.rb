c2553c263122c1a9b405f1641517bce5f6bd99e8
VZbitnieva <v.zbitnieva@gmail.com>
2023-07-20 12:05:10 +0300

    checking the number for evenness

---
+++ RUBY/Part_I/lesson7/3_even_odd.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/classes/object'
+
+num = ''
+
+loop do
+  print 'Enter a number: '
+  num = gets.chomp
+  puts "#{num} - is not a number" unless num.numeric?
+  break if num.numeric?
+end
+
+num = num.to_i
+
+if num.even?
+  puts "#{num} - even number"
+else
+  puts "#{num} - odd number"
+end
