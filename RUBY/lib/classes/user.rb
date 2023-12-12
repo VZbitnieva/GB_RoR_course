d606cb96b5caf4ef9343a9cf89af17fab910d5c6
Vira <v.zbitnieva@gmail.com>
2023-12-12 13:44:05 +0200

    created several users

--- RUBY/lib/classes/user.rb
+++ RUBY/lib/classes/user.rb
@@ -5,14 +5,20 @@ class User
 
   # rubocop:disable Style/ClassVars
   @@counter = 0
+  @@user_list = []
   def initialize(name: '', full_name: '')
     @name = name
     @full_name = full_name
     @@counter += 1
+    @@user_list << self
   end
 
   def self.quantity
     @@counter
+  end
+
+  def self.list
+    @@user_list
   end
 end
 # rubocop:enable Style/ClassVars
